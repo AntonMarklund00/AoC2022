f2b1fc045e54eacfeca424a85a2615961e9ef2c5
Anton Marklund <anton.marklund00@gmail.com>
2023-12-29 15:41:34 +0100

    day-03 part one

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -1,3 +1,22 @@
 fn main(){
     let input = include_str!("./input.txt");
+    let mut sum = 0;
+    for line in input.lines() {
+        let split = line.split_at(line.len()/2);
+        let mut common: Vec<&str> = Vec::new();
+        for item in split.0.split(""){
+            if split.1.contains(&item) && !common.contains(&item){
+                common.push(item);
+            }
+        }
+
+        let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect::<Vec<char>>();
+        for item in common{
+            if item != "" {
+                sum += alphabet.iter().position(|&r| r.to_string() == item).unwrap()+1;
+            }
+        }
+    }
+
+    println!("{}", sum);
 }
