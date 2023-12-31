f261dd83858735d921af1335f0d86990a7ad4b2e
Anton Marklund <anton.marklund00@gmail.com>
2023-12-31 23:09:48 +0100

    day-03 part two

--- day-03/src/bin/part2.rs
+++ day-03/src/bin/part2.rs
@@ -1,3 +1,24 @@
 fn main(){
-    let input = include_str!("./input.txt");
+    let input: Vec<&str> = include_str!("./input.txt").split("\n").collect();
+    let data: Vec<Vec<&str>> = input.chunks(3).map(|s| s.to_vec()).collect();
+
+    let mut common: Vec<char> = Vec::new();
+
+    for i in data {
+        let mut chunk_common: Vec<char> = Vec::new();
+        for j in i.get(0).unwrap().chars() {
+            if i.get(1).unwrap().contains(j) && i.get(2).unwrap().contains(j) && !chunk_common.contains(&j) {
+                chunk_common.push(j);
+            }
+        }
+        common.append(&mut chunk_common);
+
+    }
+    let mut sum = 0;
+    let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect::<Vec<char>>();
+    for item in common{
+        sum += alphabet.iter().position(|&r| r == item).unwrap()+1;
+    }
+
+    println!("{}", sum);
 }
